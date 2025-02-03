208b12aaca7270dd8e0164f7f12aab98efca54f9
David Enikuomehin <enikuauth@gmail.com>
2025-02-03 01:55:18 +0100

    feat: update app to include theme schema

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_app/src/screens/home_page.dart';
 import 'package:todo_app/src/providers/todo_provider.dart';
+import 'package:todo_app/src/themes/app_theme.dart';
 
 void main() {
   runApp(ChangeNotifierProvider(
     create: (context) => TodoProvider(),
-    MaterialApp(title: "Todo App", home: const HomePage())));
+    MaterialApp(title: "Todo App",  
+    theme: AppTheme.lightTheme,
+        darkTheme: AppTheme.darkTheme,
+        home: const HomePage())));
 }
---
+++ lib/src/themes/app_theme.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData lightTheme = ThemeData(
+    primarySwatch: Colors.blue,
+    brightness: Brightness.light,
+    appBarTheme: const AppBarTheme(
+      elevation: 0,
+      centerTitle: true,
+    ),
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: Colors.blue,
+    ),
+    cardTheme: CardTheme(
+      elevation: 2,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+    ),
+  );
+
+  static ThemeData darkTheme = ThemeData(
+    primarySwatch: Colors.blue,
+    brightness: Brightness.dark,
+    appBarTheme: const AppBarTheme(
+      elevation: 0,
+      centerTitle: true,
+    ),
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: Colors.blue,
+    ),
+    cardTheme: CardTheme(
+      elevation: 2,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+    ),
+  );
+}
